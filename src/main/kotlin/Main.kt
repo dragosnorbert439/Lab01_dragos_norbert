aaef1f917e5e99065bb444dca832c2255dea6ad2
drágosnorbert <79311672+dragosnorbert439@users.noreply.github.com>
2021-09-29 23:55:25 +0300

    8/8 ex 4 - 2nd decoding throws error: "Illegal base64 character 20"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
-import java.beans.Encoder
-import java.lang.StringBuilder
+import java.util.*
+import kotlin.reflect.KFunction1
 
 fun main(args: Array<String>)
 {
@@ -12,7 +12,7 @@ fun main(args: Array<String>)
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     // 2
-    val daysOfTheWeek = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    var daysOfTheWeek = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
     for (day in daysOfTheWeek) print("$day ")
     println()
@@ -39,7 +39,65 @@ fun main(args: Array<String>)
     println("Encoded: $msg")
     msg = myDecode(msg)
     println("Decoded: $msg")
+
+    println("Encoded again: ${messageCoding(msg, ::myEncode)}")
+    println("Decoded again: ${messageCoding(msg, ::myDecode)}")
     println()
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    // 5
+    val intList = mutableListOf( 1, 2, 3, 4, 5, 439, 48, 42, 256, 17 )
+    println("Only even elements list: ${myFilter(intList).toString()}")
+    println()
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    // 6
+    println("Before: ${intList.toString()}")
+    var intListDoubled = mutableListOf<Int>()
+    intList.map { intListDoubled.add(it).and(intListDoubled.add(it)) }
+    println("After: ${intListDoubled.toString()}")
+    println()
+
+    println("All uppercase: ${daysOfTheWeek.map { it.uppercase() }}")
+    println()
+
+    println("First characters: ${daysOfTheWeek.map { it.first().lowercase() }}")
+    println()
+
+    println("Lengths: ${daysOfTheWeek.map { it.length }}")
+    println()
+
+    println("Avg chars: ${daysOfTheWeek.map { it.length }.sum().toFloat() / daysOfTheWeek.map { it }.count()}")
+    println()
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    // 7
+    daysOfTheWeek = daysOfTheWeek.toMutableList()
+    daysOfTheWeek = daysOfTheWeek.filter { !it.contains('n') }
+
+    daysOfTheWeek.withIndex().map { println("Item at ${it.index} is ${it.value}") }
+    println()
+
+    println("Sorted: ${daysOfTheWeek.sorted()}")
+    println()
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    // 8
+    var iArray = IntArray(10) { (0..100).random() }
+    print("IntArray: "); iArray.forEach { print("${ it.toString() } ")}; println()
+
+    println("Sorted IntArray: ${iArray.sorted().toString()}")
+
+    var hasEven = false
+    iArray.forEach { if(it % 2 == 0) { hasEven = true } }
+    println("Contains any even number? => ${if (hasEven) "Yes" else "No" }")
+
+    var hasOdd = false
+    iArray.forEach { if(it % 2 != 0) { hasOdd = true } }
+    println("Contains only even number(s)? => ${if (hasOdd) "No" else "Yes" }")
+
+    var sum = 0; iArray.forEach { sum += it }
+    println("Average generated number: ${ sum.toFloat() / iArray.size }")
 }
 
 // for 3rd
@@ -64,12 +122,11 @@ fun isPrime(num :Int) : Boolean
 }
 
 // for 4th
-fun myEncode(str :String) : String
-{
-    return str
-}
+fun myEncode(str :String) : String = Base64.getEncoder().encodeToString(str.toByteArray())
 
-fun myDecode(str :String) : String
-{
-    return str
-}
+fun myDecode(str :String) : String = String(Base64.getDecoder().decode(str.toByteArray()))
+
+fun messageCoding(msg: String, func: KFunction1<String, String>): String = func(msg)
+
+// for 5th
+fun myFilter(list : List<Int>) : List<Int> = list.filter { it.and(1) == 0 }
